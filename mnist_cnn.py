c27b33a0301c669850343c38a7f09679bcdafef1
jamiegrain <jamiegrain@gmail.com>
2018-09-09 21:28:08 +0100

    Started the CNN approach

---
+++ mnist_cnn.py
@@ -0,0 +1,32 @@
+import tensorflow as tf 
+import numpy as np 
+import pandas as pd 
+
+df = pd.read_csv('train.csv')
+
+labels = df['label']
+df.drop(['label'], axis = 1, inplace = True)
+
+X = np.array(df)
+y = np.array(labels)
+
+X = tf.keras.utils.normalize(X, axis=1)
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(256, (3,3), activation = tf.nn.relu, input_shape=(28, 28, 3)))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+
+model.add(tf.keras.layers.Conv2D(256, (3,3), activation = tf.nn.relu))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+
+model.add(tf.keras.layers.Flatten())
+
+model.add(tf.keras.layers.Dense(64, activation = tf.nn.relu))
+
+model.add(tf.keras.layers.Dense(1, activation = tf.nn.sigmoid))
+
+model.compile(loss='binary_crossentropy',
+	optimizer='adam',
+	metrics=['accuracy'])
+
+model.fit(X, y, epochs = 5, validation_split = 0.2)
