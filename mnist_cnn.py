fed121d8d02547479056aa9dea95844968731645
Jamie Grain <jamiegrain@gmail.com>
2018-09-13 18:07:14 +0100

    Changed some things

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -12,16 +12,18 @@ y = np.array(labels)
 
 X = tf.keras.utils.normalize(X, axis=1)
 
+X.reshape(-1, 28, 28, 1)
+
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Conv2D(256, (3,3), activation = tf.nn.relu, input_shape=(28, 28, 3)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation = tf.nn.relu, input_shape=X.shape[1:]))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
 
-model.add(tf.keras.layers.Conv2D(256, (3,3), activation = tf.nn.relu))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation = tf.nn.relu))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
 
 model.add(tf.keras.layers.Flatten())
 
-model.add(tf.keras.layers.Dense(64, activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(32, activation = tf.nn.relu))
 
 model.add(tf.keras.layers.Dense(1, activation = tf.nn.sigmoid))
 
