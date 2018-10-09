b43cc444d17b6c3dc2c86119ffd4854d20d55f2c
jamiegrain <35873917+jamiegrain@users.noreply.github.com>
2018-10-09 13:18:11 +0100

    Changed input shape

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -10,12 +10,9 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 x_train = np.reshape(x_train, (60000, 28, 28, 1))
 x_test = np.reshape(x_test, (10000, 28, 28, 1))
 
-X.reshape(-1, 28, 28, 1)
-
 model = tf.keras.Sequential()
 
-model.add(tf.keras.layers.Conv2D(100, (3,3), activation = tf.nn.relu, input_shape=(28, 28, 1)))
-model.add(tf.keras.layers.Conv2D(64, (3,3), activation = tf.nn.relu, input_shape=X.shape[1:]))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation = tf.nn.relu, input_shape=(-1, 28, 28, 1))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
 
 model.add(tf.keras.layers.Conv2D(64, (3,3), activation = tf.nn.relu))
